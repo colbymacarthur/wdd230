15e1a22ef958df0ec2ae45a605bae9a028bbd1bd
colbymacarthur <colby.mac89@gmail.com>
2024-05-05 15:32:47 -0600

    footer update

---
+++ chamber/scripts/script.js
@@ -0,0 +1,4 @@
+// get last updated date
+document.getElementById("lastModified").textContent = "Last Modified: " + new Date().toLocaleDateString();
+// get todays year
+document.getElementById("year").innerHTML = new Date().getFullYear();
