021d38e4ef615fd62b62150662ef8fdf7c5d46a6
colbymacarthur <colby.mac89@gmail.com>
2024-05-04 23:39:05 -0600

    Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,21 @@
 document.getElementById("lastModified").textContent = "Last Modified: " + new Date().toLocaleDateString();
 // get todays year
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
