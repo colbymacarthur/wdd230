72afb3f6607a5b62bd5655f1ff1dd584144702f4
colbymacarthur <colby.mac89@gmail.com>
2024-05-11 12:55:31 -0600

    week 2 fix

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -2,3 +2,12 @@
 document.getElementById("lastModified").textContent = "Last Modified: " + new Date().toLocaleDateString();
 // get todays year
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// hamburger button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
