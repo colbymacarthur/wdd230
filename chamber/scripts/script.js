66b3bce1ae480f5451ca3e445fff1e4352b5e0e6
colbymacarthur <colby.mac89@gmail.com>
2024-05-13 21:30:34 -0600

    Update discover

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -11,3 +11,17 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// get visits
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome to my home page.`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
+
+const todayDisplay = document.querySelector(".today");
+let 
